41f8758b3555c7c721f4f7108b09fa1eefbe0515
maurormrz <maurormrz15@gmail.com>
2023-09-26 11:26:39 -0300

    Initial commit

---
+++ Historias-Locas/historias-locas.py
@@ -0,0 +1,11 @@
+nombre = input("Ingresa tu Nombre: ") # Nombre
+apellido = input("Ingresa tu Apellido: ") # Apellido
+edad = input("Ingresa tu Edad: ") # Edad
+ubicacion = input("Ingresa tu Ubicación: ") # Ubicación
+estudio = input("Ingresa que estas estudiando: ") # Estudio
+
+print("HISTORIAS LOCAS")
+
+historialoca = f"¡Hola, un gusto conocerte!. Me llamo {nombre}, mi apellido es {apellido} y tengo {edad} años de edad. Actualmente estoy viviendo en {ubicacion} en donde estoy estudiando la carrera de {estudio}. ¡Y con gusto te llevaré a conocer mi ciudad!."
+
+print(historialoca)
