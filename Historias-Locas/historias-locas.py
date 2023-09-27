6778f435b988b4edaed6219a0ee8a8d9d8a07cb9
maurormrz <maurormrz15@gmail.com>
2023-09-27 11:08:08 -0300

    v-0.0.2
    Comentarios añadidos.

--- Historias-Locas/historias-locas.py
+++ Historias-Locas/historias-locas.py
@@ -1,9 +1,16 @@
-nombre = input("Ingresa tu Nombre: ")
-apellido = input("Ingresa tu Apellido: ")
-edad = input("Ingresa tu Edad: ")
-ubicacion = input("Ingresa tu Ubicación: ")
-estudio = input("Ingresa que estas estudiando: ")
+# Colección de datos para crear la historia.
+
+nombre = input("Ingresa tu Nombre: ") # Input nombre.
+apellido = input("Ingresa tu Apellido: ") # Input apellido.
+edad = input("Ingresa tu Edad: ") # Input edad.
+ubicacion = input("Ingresa tu Ubicación: ") # Input ubicacion.
+estudio = input("Ingresa que estas estudiando: ") # Input estudio.
+
+# Título del juego.
+
 print("HISTORIAS LOCAS")
+
+# Variable con f-string (reemplaza con los datos ingresados).
 
 historialoca = f"¡Hola, un gusto conocerte!. Me llamo {nombre}, mi apellido es {apellido} y tengo {edad} años de edad. Actualmente estoy viviendo en {ubicacion} en donde estoy estudiando la carrera de {estudio}. ¡Y con gusto te llevaré a conocer mi ciudad!."
 
